07e1938516b30a511f19e538306b46dfd3f1dc79
thecalmmoneymindset-art <thecalmmoneymindset@gmail.com>
2026-05-24 17:44:08 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/app/',
   build: {
-    outDir: 'dist/app',
+    outDir: 'dist',
     emptyOutDir: true,
   },
 })
